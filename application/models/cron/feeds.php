496bf2307e1eb873385d6bed2aa7ba485d48b108
Fohlen <fohlen@einhufer.info>
2015-02-06 21:13:27 +0100

    Added a model for the feed.

--- application/models/cron/feeds.php
+++ application/models/cron/feeds.php
@@ -8,21 +8,39 @@ class Feeds extends CI_Model
 	
 	public function getFeedList()
 	{
-		
+		return $this->db->get('feeds');
 	}
 		
 	public function getLatestItemDate($id)
 	{
+		// Use database caching because this will be called quiet often within the cron-controller
+		$this->db->start_cache();
+		$this->db
+		->select('date')
+		->from('feed_items')
+		->where('id', $id)
+		->group_by('date')
+		->limit(1);
+		$this->db->stop_cache();
 		
+		return $this->db->get();
 	}
 	
 	public function addItem($id, $Item)
 	{
+		$data = array(
+				'title' => $Item['title'],
+				'link' => $Item['link'],
+				'date' => $Item['date'],
+				'description' => $Item['description']
+		);
 		
+		$this->db->insert('feed_items', $data);		
 	}
 	
-	public function updateItem($id, $Item)
+	/* TODO: Update items if they're changed afterwards ... is this necessary?
+	 * public function updateItem($id, $Item)
 	{
 		
-	}
+	}*/
 }
