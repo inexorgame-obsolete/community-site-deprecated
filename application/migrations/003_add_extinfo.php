67714b02ee1e03013f97c1d882f65d9e96056ca7
Fohlen <fohlen@einhufer.info>
2015-02-11 19:18:50 +0100

    Add a standard migration for the extinfo cronjob -> bump this before the authentication system.

---
+++ application/migrations/003_add_extinfo.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_Add_extinfo extends CI_Migration
+{
+	public function up()
+	{
+		
+	}
+	
+	public function down()
+	{
+		
+	}
+}
