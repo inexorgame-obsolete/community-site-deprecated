0d1917da897b828d1bde2e4a5dd842ffa621e633
Fohlen <fohlen@einhufer.info>
2015-02-11 19:03:46 +0100

    Added getEntriesInRange (which will be used on the main site..)

--- application/models/news.php
+++ application/models/news.php
@@ -30,4 +30,43 @@ class News_Model extends CI_Model
 		}
 		
 	}
+	
+	/**
+	 * getEntriesInRange
+	 * This function returns all item ID's within the pubDate range of startDate - endDate
+	 * Input can either be a DateTime object or a string
+	 * 
+	 * @param mixed $startDate
+	 * @param mixed $endDate
+	 * @return mixed
+	 */
+	public function getEntriesInRange($startDate, $endDate)
+	{
+		// Make sure both datetime strings are converted to UTC
+		$gmtTimezone = new DateTimeZone('GMT');
+		
+		if (!$startDate instanceof DateTime)
+		{
+			$startDate = new DateTime($startDate, $gmtTimezone);
+		}
+		
+		if (!$endDate instanceof DateTime)
+		{
+			$endDate = new DateTime($endDate, $gmtTimezone);
+		}
+		
+		$this->db
+		->select('id')
+		->where('pubDate >=', $startDate->format("Y-m-d H:i:s"))
+		->where('pubDate <=', $endDate->format("Y-m-d H:i:s"));
+		
+		$query = $this->db->get();
+		
+		if ($query->num_rows > 0)
+		{
+			return $query->result();
+		} else {
+			return false;
+		}
+	}
 }
